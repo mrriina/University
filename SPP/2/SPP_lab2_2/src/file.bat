775e066195da755c370e8396fc629d0c7518cf7c
mrriina <100038775+mrriina@users.noreply.github.com>
2023-01-10 09:59:53 +0300

    Add files via upload

---
+++ SPP/2/SPP_lab2_2/src/file.bat
@@ -0,0 +1,1 @@
+java -jar C:\Users\Марина\Desktop\Univers\3Course\1semester\SPP\2\SPP_lab2_2\out\artifacts\SPP_lab2_2_jar\SPP_lab2_2.jar uniq u D:\sppLabs\2\SPP_lab2_2\src\start.txt D:\sppLabs\2\SPP_lab2_2\src\end.txt

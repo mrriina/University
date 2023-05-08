23ce397280ba72242f0a6e4a8226f3ae41dd91f5
mrriina <mariaip12345678911@gmail.com>
2023-05-08 17:34:12 +0300

    PIS

--- PIS/src/main/java/com/example/ulbitvspring/exception/UserAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ulbitvspring.exception;
-
-public class UserAlreadyExistException extends Exception {
-    public UserAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- PIS/src/main/java/com/example/ulbitvspring/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ulbitvspring.exception;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
